b2b089233019130fcf6e69a3d63c789c4069d35f
Neil Thomas <neilthomas@berkeley.edu>
2025-09-18 12:54:28 -0700

    File updated at 9/18/2025, 12:54:27 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,10 +1,10 @@
 """
 Problem: 1-TwoSum
-Last updated: 9/17/2025, 7:02:36 PM
+Last updated: 9/18/2025, 12:54:27 PM
 URL: https://leetcode.com/problems/two-sum/
 Difficulty: Easy
 Runtime: 0 ms (Beats 100%)
-Memory: 19 MB (Beats 23.65%)
+Memory: 19.2 MB (Beats 10.32%)
 Language: python3
 """
 
@@ -16,5 +16,7 @@ class Solution:
             if target - num in pair_idx:
              return [i, pair_idx[target - num]]
             pair_idx[num] = i
+        
 
 
+        
