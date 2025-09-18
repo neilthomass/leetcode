9be7423e0b8086516005fbeb55bf08557c80e8ef
Neil Thomas <neilthomas@berkeley.edu>
2025-09-17 18:07:51 -0700

    Create 1585-TheKthFactorOfN

---
+++ 1585-TheKthFactorOfN/1585-TheKthFactorOfN.py
@@ -0,0 +1,12 @@
+# Last updated: 9/17/2025, 6:07:51 PM
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+
+        
+        for i in range(1, n + 1):
+            if n % i == 0:
+                k -= 1
+            if k == 0:
+                return i
+        return -1 
+        
