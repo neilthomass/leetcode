b3898b561e7989d32863ac6e05b1c8ca1e3a052c
Neil Thomas <neilthomas@berkeley.edu>
2025-09-18 20:38:32 -0700

    Update solution: 0001. Two Sum
    Language: java | Runtime: 2 ms (98.89%) | Memory: 45.1 MB (31.42%)

--- java/0001 two-sum.java
+++ java/0001 two-sum.java
@@ -1,15 +1,14 @@
 /**
- * Last updated: 9/18/2025, 2:03:25 PM
+ * Last updated: 9/18/2025, 8:38:32 PM
  * URL: https://leetcode.com/problems/two-sum/
  * Runtime: 2 ms (Beats 98.89%)
- * Memory: 44.9 MB (Beats 65.12%)
+ * Memory: 45.1 MB (Beats 31.42%)
  * Language: java
  */
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> pairIdx = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             if (pairIdx.containsKey(target - num)) {
