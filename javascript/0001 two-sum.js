c1a0058456715ebe2a2998f7f0bfaf1734912d56
Neil Thomas <neilthomas@berkeley.edu>
2025-09-18 20:15:23 -0700

    Add solution: 0001. Two Sum
    Language: javascript | Runtime: 2 ms (98.89%) | Memory: 45.2 MB (31.42%)

---
+++ javascript/0001 two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * Last updated: 9/18/2025, 8:15:23 PM
+ * URL: https://leetcode.com/problems/two-sum/
+ * Runtime: 2 ms (Beats 98.89%)
+ * Memory: 45.2 MB (Beats 31.42%)
+ * Language: javascript
+ */
+
+// O(n) - One-pass Hash Table
+var twoSum = function(nums, target) {
+    let map = new Map;
+    for (var i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i]
+        }
+        map.set(nums[i], i);
+    }
+}
